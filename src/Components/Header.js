0bc2caf37be23e75fbee0b15017e31bfd5886ad7
Sandeep Painkra <samdeeppainkra@gmail.com>
2022-04-17 01:03:43 +0530

    second commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <div className="header_leftIcon">
+          <h5>icons</h5>
+        </div>
+      </div>
+      <div className="header_right">
+        <h1 className="logo">hulu</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
